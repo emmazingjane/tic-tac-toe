072d681fd2b7bdde5cdadf30d41b70bb4546876e
emmabennett <emma.j.bennett15@gmail.com>
2018-03-20 16:12:55 -0700

    got to alternate and did basic setup of box

--- js/app.js
+++ js/app.js
@@ -2,5 +2,33 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  console.log('java')
+
+  var currentTurn = 'X' 
+  function alternate(){
+  	if (currentTurn === 'X'){
+  		currentTurn = 'O'
+  		return 
+  	} else if (currentTurn === 'O') {
+  		currentTurn = 'X'
+  		return
+  	}
+  	
+  }
+
+
+  // 
+
+  $('.col-md-4.box').on('click', function(){
+  	// $('#a').html('<img src="http://i61.tinypic.com/faz9g3.png"/>')
+  	$(this).text(currentTurn);
+  	alternate()
+  })
+
+
+
+
+
+
 
 });
