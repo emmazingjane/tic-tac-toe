b35683844df6194021c621b4704a45749a79c034
emmabennett <emma.j.bennett15@gmail.com>
2018-03-21 08:22:13 -0700

    finished logic

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   console.log('java')
 
   var currentTurn = 'X' 
+
   function alternate(){
   	if (currentTurn === 'X'){
   		currentTurn = 'O'
@@ -16,14 +17,25 @@ $(document).ready(function() {
   	
   }
 
-
-  // 
+ 
 
   $('.col-md-4.box').on('click', function(){
-  	// $('#a').html('<img src="http://i61.tinypic.com/faz9g3.png"/>')
+  		// if (.col-md-4.box === " ") {
+
+			if (this.textContent !== "") {
+				return
+			}
   	$(this).text(currentTurn);
-  	alternate()
-  })
+  	alternate();
+  
+})
+
+  	// $('#a').html('<img src="http://i61.tinypic.com/faz9g3.png"/>')
+$(".btn-primary").click(function() {
+  $('.col-md-4.box').empty();
+  currentTurn = 'X';
+});
+ 
 
 
 
